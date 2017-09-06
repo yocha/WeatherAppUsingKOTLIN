a41ccd343752603f778afa0398cc8a4608b40db6
yocha <yocha@yahoo.com>
2017-09-06 13:52:30 -0700

    use response from myjson

--- app/src/main/java/com/upwiththekite/weather/CityWeather.kt
+++ app/src/main/java/com/upwiththekite/weather/CityWeather.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CityWeather : AppCompatActivity() {
 
@@ -18,5 +21,22 @@ class CityWeather : AppCompatActivity() {
         var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,myList)
 
         listView.adapter = adapter
+
+        var retriver = WeatherRetriver()
+
+        val callback = object : Callback<List<Forecast>> {
+            override fun onResponse(call: Call<List<Forecast>>?, response: Response<List<Forecast>>?) {
+                println("We got a response")
+                println(response?.body())
+                for (forecastDay in response!!.body()!!) {
+                    println("High:${forecastDay.high} Low:${forecastDay.low}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Forecast>>?, t: Throwable?) {
+                println("We got a fail")
+            }
+        }
+        retriver.getForecast(callback)
     }
 }
